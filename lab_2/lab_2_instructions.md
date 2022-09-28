982d1ec646180cdd3c451d5c4906a06e96cd71b6
Tobias Fors <tobias.fors.1993@gmail.com>
2022-09-29 00:19:07 +0200

    upload instructions

---
+++ lab_2/lab_2_instructions.md
@@ -0,0 +1,28 @@
+# Laboration 2 
+## LÄS DETTA FÖRST
+**Deadline**: 2022/10/9 23:59 (Söndag den 9e oktober)
+**Komplettering 1 Deadline**: 2022/10/16 23:59 (Söndag den 16e oktober)
+**Komplettering 2 Deadline**: 2022/10/23 23:59 (Söndag den 23e oktober)
+**Betyg**: U, G, VG
+
+- För att få G ska applikationen fungera och vara relativt buggfri, du ska hantera errors i större grad.
+
+- För att få VG ska du implementera ytterligare funktionalitet och applikationen ska i stort sätt vara helt buggfri. De metoder som möjliggör VG-betyget specificerar vad du behöver göra.
+
+## Inlämning
+Ta bort alla onödiga filer, din kod bör vara på den huvudsakliga branchen "main" eller "master".
+Se till att du körde git clone på din EGEN repository för labben.
+Feedback ges via en speciell branch som skapas automatiskt, mer information om detta får du av utbildaren.
+
+## Beskrivning:
+
+Din chef har påbörjat en enkel applikation för att kunna konvertera valutor. 
+Ditt uppdrag är att färdigställa applikationen utifrån instruktionerna givna i filen.
+
+Sammanfattningsvis kommer du att behöva:
+- Registrera ett gratiskonto på [https://openexchangerates.org/](https://openexchangerates.org/) - Du har totalt 1000 anrop på en månad och och kommer få en API-nyckel tilldelad. Du hittar API-nyckeln på "Your dashboard" -> "App IDs", du kommer sedan använda den när du ska göra anrop till deras API. Klistra in app-ID't under variabeln app_id som du hittar i kodspecifikationen i filen main.py. Var försiktig med eventuella loopar!
+- Implementera en meny som ger användaren val, hur menyn ska se ut specificeras i filen.
+- Implementera befintliga metoder som är specificerade. Om du vill lägga till ytterligare klasser, metoder eller filer ska det finnas tydliga och logiska skäl till detta.
+
+**Du ska läsa specifikationen NOGGRANT innan du börjar. Det innebär att innan du skriver första raden kod ska du bilda dig en helhetsbild genom att ha läst beskrivningen under samtliga metoder.**
+
---
+++ lab_2/main.py
@@ -0,0 +1,78 @@
+import requests # This needs to be installed with pip
+# DO NOT UPLOAD A VIRTUAL ENVIRONMENT TO GIT
+
+class CurrencyConverter:
+    def __init__(self):
+        """
+        When a currency converted is created it should first try to load currency data from JSON (using the load_currency_data-method)
+        If it doesn't find any json-data, it should try to get the data from the exchangerates API (using the fetch_currency_data-method)
+        """
+        pass
+    
+    def fetch_currency_data():
+        """
+        This method should fetch new currency data using the openexchangerate API
+        It has some boilerplate code you can use.
+        """
+        # Use this code to fetch currency data from openexchangerates.org.
+        app_id = "YOUR_APP_ID" # Add your own app_id from openexchangerates.org here
+        url = f"https://openexchangerates.org/api/latest.json?app_id={app_id}"
+        headers = {"accept": "application/json"} # This needs to be added, it tells the API that they should return JSON
+        response = requests.get(url, headers=headers)
+        pass
+    
+    def convert_from_usd(self, to_currency):
+        """
+        This method should convert from USD to a currency of choice
+        """
+        pass
+            
+    def convert_any_currency(self, from_currency, to_currency):
+        """
+        This method is not required for Godkänt (G) grade
+                
+        # # # # # # FÖR VÄL GODKÄND - VG REQUIREMENT # # #
+        This method converts from any currency, to any currency
+        This should not require any more requests (e.g you do not need to make another request with a different base currency)
+        """
+        pass
+    
+    def list_currencies(self):
+        """
+        This method lists available currencies in alphabetical order
+        """
+        pass
+    
+    def load_currency_data(self):
+        """
+        # # # # # FÖR GODKÄND - G REQUIREMENT # # # # #:
+        This method should attempt to load currency data from a json-file.
+        
+        # # # # # FÖR ATT FÅ VÄL GODKÄNT - VG REQUIREMENT # # # # # #
+        The function should look at the current time using the python datetime module (https://docs.python.org/3/library/datetime.html).
+        If the saved data is older than more hour, it should automatically update the data being used and save it.
+        This is possible by looking at the timestamp of the fetched data. You need to figure out yourself how this can be done.
+        """
+        pass
+
+    def export_to_json(self):
+        """
+        This should export to JSON so that the application can be run again without the need to fetch new data
+        """
+        pass
+    
+def main():
+    """
+    This function should contain a menu allowing the user to
+    [0] - (G) List all currencies
+    [1] - (G) Convert USD to a currency of choice
+    [2] - (G) Refresh the data (fetch new currency data, this is because the API updates new currency rates every hour)
+    [3] - (G) Export the data to JSON
+    [4] - (VG) Convert from any currency to any currency that is available on the API
+    """
+    pass
+    
+if __name__ == "__main__":
+    # If you don't understand what this is or does, google / ask around, this was covered during lectures
+    main()
+    
